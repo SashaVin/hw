4c12bac65885c8d3be05d5b3ddb781695f7472e5
SashaVin <55402145+SashaVin@users.noreply.github.com>
2021-09-08 19:01:51 +0700

    Update first.java

--- first.java
+++ first.java
@@ -1,1 +1,16 @@
+class Scratch {
+    public static void main(String[] args) {
+        int[] array = {1, 2};
+        System.out.print(rekursia(array, 0, 0));
+    }
+    public static int rekursia(int[] array, int sum, int counter) {
+        if (array.length == counter) {
+            return sum;
+        } else {
+            sum += array[counter];
+            counter += 1;
+            return rekursia(array, sum, counter);
+        }
 
+    }
+}
